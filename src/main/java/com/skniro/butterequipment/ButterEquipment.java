621c565033adcdfdce490f30eebcf6b81f66f510
skniro <46392259+skniro@users.noreply.github.com>
2023-08-01 16:57:16 +0800

    1.0.0

---
+++ src/main/java/com/skniro/butterequipment/ButterEquipment.java
@@ -0,0 +1,15 @@
+package com.skniro.butterequipment;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class ButterEquipment implements ModInitializer {
+
+
+    @Override
+    public void onInitialize() {
+    }
+}
